d1ad0e0346991d6d5a96ac8cacbaa4a6fd44ffda
Nuwanfav <nuwanmadusanka.epc@gmail.com>
2019-08-13 15:00:39 +0530

    migrations

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class client(models.Model):
+    client_name = models.CharField(max_lenght=100)
+    logo = models.ImageField(upload_to='images/%Y%m/%d/')
+    def __str__(self):
+        return self.logo
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from clients.models import client
 
-# Create your models here.
+class project(models.Model):
+    client_name = models.ForeignKey(client, on_delete=models.DO_NOTHING)
+    pro = models.CharField(max_length=100)
+    pro_role = models.CharField(max_length=100)
+    pro_photo = models.ImageField(upload_to='images/%Y%m/%d/')
+    is_published = models.BooleanField(default=True)
+    def __str__(self):
+        return self.pro
