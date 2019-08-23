75493ad7a88c2c19d3084e2b351d82b0d5500343
Nuwanfav <nuwanmadusanka.epc@gmail.com>
2019-08-23 13:01:33 +0530

    migrations

--- clients/models.py
+++ clients/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from datetime import date
 
 class client(models.Model):
-    client_logo : models.ImageField(upload_to='images/%Y/%m/%d/')
+    client_id = models.AutoField(primary_key=True)
+    client_logo = models.ImageField(upload_to='images/%Y/%m/%d/')
     def __str__(self):
         return self.client_logo
--- team/models.py
+++ team/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class member(models.Model):
-    member_first_name : models.CharField(max_length=100)
-    member_last_name : models.CharField(max_length=100)
-    member_designation_1 : models.CharField(max_length=100)
-    member_designation_2 : models.CharField(max_length=100)
-    member_designation_3 : models.CharField(max_length=100)
+    member_id = models.AutoField(primary_key=True)
+    member_first_name = models.CharField(max_length=100)
+    member_last_name = models.CharField(max_length=100)
+    member_designation_1 = models.CharField(max_length=100)
+    member_designation_2 = models.CharField(max_length=100)
+    member_designation_3 = models.CharField(max_length=100)
     member_photo : models.ImageField(upload_to='images/%Y/%m/%d/')
     def __str__(self):
         return self.member_photo
