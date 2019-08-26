baa1cdddfa5dab939502a52b0c7234484c8e1654
Lasantha Premarathna <lasanthpremarathna@gmail.com>
2019-08-26 19:12:20 +0530

    Updated

--- projects/models.py
+++ projects/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from clients.models import client
+# from clients.models import client
 
-class project(models.Model):
-    client_name = models.ForeignKey(client, on_delete=models.DO_NOTHING)
-    pro = models.CharField(max_length=100)
-    pro_role = models.CharField(max_length=100)
-    pro_photo = models.ImageField(upload_to='images/%Y%m/%d/')
-    is_published = models.BooleanField(default=True)
-    def __str__(self):
-        return self.pro
+# class project(models.Model):
+#     client_name = models.ForeignKey(client, on_delete=models.DO_NOTHING)
+#     pro = models.CharField(max_length=100)
+#     pro_role = models.CharField(max_length=100)
+#     pro_photo = models.ImageField(upload_to='images/%Y%m/%d/')
+#     is_published = models.BooleanField(default=True)
+#     def __str__(self):
+#         return self.pro
